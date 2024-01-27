b2159f160e5c5b85ec338bff65477038794735b5
LuizGiserman <luizgiserman@poli.ufrj.br>
2024-01-27 13:52:12 -0300

    Update deno

--- sections/Header/HeaderCaroussel.tsx
+++ sections/Header/HeaderCaroussel.tsx
@@ -1,6 +1,5 @@
 
 import { Section } from "deco/blocks/section.ts";
-import { renderSection } from "deco/pages/LivePage.tsx";
 
 export interface Props {
   header: Section
@@ -8,12 +7,12 @@ export interface Props {
   headlineText: string
   subText: string
 }
-function HeaderCaroussel ({header,carousel, headlineText, subText}: Props){
+function HeaderCaroussel ({header: {Component: CompHeader, props: propsHeader}, carousel: {Component: CompCarousel, props: propsCarousel}, headlineText, subText}: Props){
   return (
     <div class="relative flex justify-center content">
-        {renderSection(carousel, 1)}
+          <CompCarousel {...propsCarousel}/>
         <div class="absolute top-0 left-0">
-            {renderSection(header, 0)}
+            <CompHeader {...propsHeader}/>
         </div>
         <div class="hidden md:flex flex-col absolute justify-center w-[32rem] -mt-64 self-center" >
           <span class="text-white font-extrabold md:text-5xl 2xl:text-6xl">{headlineText}</span>
